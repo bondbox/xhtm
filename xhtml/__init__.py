ba1a6d82a9d218a30c208bb54633a090eec4b7d0
Mingzhe Zou <zoumingzhe@qq.com>
2025-03-14 10:27:32 +0800

    add request

--- xhtml/__init__.py
+++ xhtml/__init__.py
@@ -3,6 +3,7 @@
 from .element import *  # noqa:F401,F403
 from .header import AcceptLanguage  # noqa:F401
 from .header import LanguageQ  # noqa:F401
+from .request import StreamResponse  # noqa:F401
 from .resource import FileResource  # noqa:F401
 from .resource import Resource  # noqa:F401
 from .template import Template  # noqa:F401
---
+++ xhtml/request/stream.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+
+from requests import Response
+
+
+class StreamResponse():
+    CHUNK_SIZE: int = 1048576  # 1MB
+
+    def __init__(self, response: Response) -> None:
+        self.__response: Response = response
+
+    @property
+    def response(self) -> Response:
+        return self.__response
+
+    def generate_content(self):
+        for chunk in self.response.iter_content(chunk_size=self.CHUNK_SIZE):
+            yield chunk
