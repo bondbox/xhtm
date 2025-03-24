6efc79a0a3f68c271a8b21555a93afba96080ee0
Mingzhe Zou <zoumingzhe@outlook.com>
2025-03-25 00:54:51 +0800

    flask: refactor FlaskProxy to extend RequestProxy

--- xhtml/flask/proxy.py
+++ xhtml/flask/proxy.py
@@ -1,54 +1,19 @@
 # coding:utf-8
 
-from typing import Dict
-from urllib.parse import urljoin
-
 from flask import Request
 from flask import Response
 from flask import stream_with_context
 from requests import ConnectionError
-from requests import Session
 
+from xhtml.request import MethodNotAllowed
+from xhtml.request import RequestProxy
 from xhtml.request import StreamResponse
 
 
-class FlaskProxy():
-
-    EXCLUDED_REQUEST_HEADERS = [
-        "connection",
-        "content-length",
-        "host",
-        "keep-alive",
-        "proxy-authorization",
-        "transfer-encoding",
-        "via",
-    ]
-
-    EXCLUDED_RESPONSE_HEADERS = [
-        "connection",
-        "content-encoding",
-        "content-length",
-        "transfer-encoding",
-    ]
+class FlaskProxy(RequestProxy):
 
     def __init__(self, target: str) -> None:
-        # self.__session: Session = Session()
-        self.__target: str = target
-
-    @property
-    def session(self) -> Session:
-        return Session()
-
-    @property
-    def target(self) -> str:
-        return self.__target
-
-    def urljoin(self, path: str) -> str:
-        return urljoin(base=self.target, url=path)
-
-    @classmethod
-    def headers(cls, request: Request) -> Dict[str, str]:
-        return {k: v for k, v in request.headers.items() if k.lower() not in cls.EXCLUDED_REQUEST_HEADERS}  # noqa:E501
+        super().__init__(target)
 
     @classmethod
     def forward(cls, sr: StreamResponse) -> Response:
@@ -67,25 +32,14 @@ class FlaskProxy():
 
     def request(self, request: Request) -> Response:
         try:
-            target_url: str = self.urljoin(request.path.lstrip("/"))
-            if request.method == "GET":
-                response = self.session.get(
-                    url=target_url,
-                    data=request.data,
-                    headers=self.headers(request),
-                    cookies=request.cookies,
-                    stream=True
-                )
-                return self.forward(StreamResponse(response))
-            elif request.method == "POST":
-                response = self.session.post(
-                    url=target_url,
-                    data=request.data,
-                    headers=self.headers(request),
-                    cookies=request.cookies,
-                    stream=True
-                )
-                return self.forward(StreamResponse(response))
+            headers = self.filter_headers({k: v for k, v in request.headers.items()})  # noqa:E501
+            response = super().request(path=request.path,
+                                       method=request.method,
+                                       data=request.data,
+                                       headers=headers,
+                                       cookies=request.cookies)
+            return self.forward(response)
+        except MethodNotAllowed:
             return Response("Method Not Allowed", status=405)
         except ConnectionError:
             return Response("Bad Gateway", status=502)
