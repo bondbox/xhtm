413aa84af749bad382494e80d1936e3204f8d671
Mingzhe Zou <zoumingzhe@qq.com>
2025-03-25 16:32:22 +0800

    bump version to 0.4.0

--- xhtml/attribute.py
+++ xhtml/attribute.py
@@ -3,7 +3,7 @@
 from urllib.parse import urljoin
 
 __project__ = "xhtm"
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 __description__ = "Rendering HTML Text"
 __urlhome__ = "https://github.com/bondbox/xhtm"
 __urlcode__ = __urlhome__
