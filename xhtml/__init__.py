961415e331f142d979af0229cada8cd0044fa336
Mingzhe Zou <zoumingzhe@qq.com>
2025-04-03 10:05:01 +0800

    header: add comprehensive HTTP headers support

--- xhtml/__init__.py
+++ xhtml/__init__.py
@@ -2,6 +2,7 @@
 
 from xhtml.element import *  # noqa:F401,F403
 from xhtml.header import AcceptLanguage  # noqa:F401
+from xhtml.header import Headers  # noqa:F401
 from xhtml.header import LanguageQ  # noqa:F401
 from xhtml.resource import FileResource  # noqa:F401
 from xhtml.resource import Resource  # noqa:F401
--- xhtml/header/__init__.py
+++ xhtml/header/__init__.py
@@ -2,3 +2,4 @@
 
 from xhtml.header.accept import AcceptLanguage  # noqa:F401
 from xhtml.header.accept import LanguageQ  # noqa:F401
+from xhtml.header.headers import Headers  # noqa:F401
