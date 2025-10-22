a807fa3f960b04758c3b01a272e28a4dfbf9f365
Mingzhe Zou <zoumingzhe@outlook.com>
2025-10-22 16:25:50 +0800

    update project

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from urllib.parse import urljoin
 
 from setuptools import find_packages
 from setuptools import setup
+from setuptools.command.install import install
 
 from xhtml.attribute import __author__
 from xhtml.attribute import __author_email__
@@ -26,6 +27,14 @@ def all_requirements():
     return requirements
 
 
+class CustomInstallCommand(install):
+    """Customized setuptools install command"""
+
+    def run(self):
+        install.run(self)  # Run the standard installation
+        # Execute your custom code after installation
+
+
 setup(
     name=__project__,
     version=__version__,
@@ -39,4 +48,8 @@ setup(
     packages=find_packages(include=["xhtml*"], exclude=["xhtml.unittest"]),  # noqa:E501
     package_data={"xhtml.resource": ["*.ico", "*.svg"],
                   "xhtml.template": ["*.html"]},
-    install_requires=all_requirements())
+    install_requires=all_requirements(),
+    cmdclass={
+        "install": CustomInstallCommand,
+    }
+)
